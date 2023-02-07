d64be1ea0f2d4af1159212ea739fc6a862459a77
ladiick <polevskux@gmail.com>
2023-02-07 14:41:41 +0300

    create Chat API

--- src/components/Communication/Communication.jsx
+++ src/components/Communication/Communication.jsx
@@ -1,11 +1,12 @@
 import s from "./Communication.module.scss";
 import Message from "../Message/Message";
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 import {setMessage} from "../../redux/slices/messageSlice";
 import {updatePeople} from "../../redux/slices/peopleSlice";
 import _ from "underscore";
+import {logDOM} from "@testing-library/react";
 
 //[people[index].recip.pk, people[index].sender.pk].includes(newMessage.sender.pk)
 // 				&&
@@ -16,32 +17,58 @@ const Communication = ({socket}) => {
 	const myId = useSelector(state => state.user.aboutUser.id)
 	const index = useSelector(state => state.people.index)
 	const people = useSelector(state => state.people.people)
+	
+	const [newMessage, setNewMessage] = useState(null);
+	
 	useEffect(() => {
 		const messageHandler = (e) => {
 			// let _ = require('underscore')
-			let newMessage = JSON.parse(e.data)
-			newMessage = newMessage.data
-			const arr1 = [newMessage.recip.pk,newMessage.sender.pk].sort()
-			const arr2 = [people[index].recip.pk,people[index].sender.pk].sort()
+			const newMessage2 = JSON.parse(e.data).data
+			// const arr1 = [newMessage.recip.pk,newMessage.sender.pk].sort()
+			dispatch(updatePeople({data: newMessage2, myId: myId}))
+			setNewMessage(newMessage2)
 			
-			dispatch(updatePeople({data: newMessage}))
-			const isEqual = _.isEqual(arr1,arr2)
-			if(isEqual) {
-				dispatch(setMessage(newMessage))
-			}
+			// const arr2 = [people[index].recip.pk,people[index].sender.pk].sort()
+			// console.log(arr1,arr2,people[index],people)
 			
+			// const isEqual = _.isEqual(arr1,arr2)
+			//
+			// if(isEqual) {
+			//
+			// 	dispatch(setMessage(newMessage))
+			// }
+			//
 		}
+		
 		socket?.addEventListener('message', messageHandler)
+		
 		return () => {
 			socket?.removeEventListener('message', messageHandler)
 		}
 	}, [socket]);
+	
+	useEffect(() => {
+		
+			if (newMessage) {
+				const arr1 = [newMessage.recip.pk, newMessage.sender.pk].sort()
+				const arr2 = [people[index].recip.pk, people[index].sender.pk].sort()
+				const isEqual = _.isEqual(arr1, arr2)
+				console.log(isEqual)
+				if (isEqual) {
+					dispatch(setMessage(newMessage))
+				}
+				
+			}
+		
+	}, [newMessage]);
+	
 	
 	
 	let message2 = [...message]
 	
 	useEffect(() => {
 		message2 = [...message]
+		
 	}, [message])
 	
 	
